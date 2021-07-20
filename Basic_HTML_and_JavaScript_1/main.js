8f927e37a6455652003c8493a91b3d83b9415933
meneses03 <87103206+meneses03@users.noreply.github.com>
2021-07-19 17:12:10 -0700

    HTML_Javascript

---
+++ Basic_HTML_and_JavaScript_1/main.js
@@ -0,0 +1,7 @@
+alert("Hello, World!");
+	function My_First_Function() {
+		var string = "Kiss me, I'm Irish!";
+		var result = string.fontcolor("green");
+		document.getElementById("Irish").innerHTML = 
+		result;
+	}
